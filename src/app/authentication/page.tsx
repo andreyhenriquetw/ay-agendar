a42b9d727a969951398dfd5f5fa7ad6a1835920e
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-03-09 22:25:41 -0300

    feat: add sign up form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+const registerSchema = z.object({
+  name: z.string().trim().min(1, { message: "O nome é obrigatório" }),
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: "O email é obrigatório" })
+    .email({ message: "Email inválido" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Senha deve ter 8 caracteres" }),
+});
+
+const AuthenticationPage = () => {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof registerSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center px-4 py-10">
+      <Tabs defaultValue="login" className="w-full max-w-md">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+
+        {/* LOGIN */}
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Faça o login para continuar.</CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-4">
+              <Input placeholder="Email" type="email" />
+              <Input placeholder="Senha" type="password" />
+            </CardContent>
+
+            <CardFooter>
+              <Button className="w-full">Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+
+        {/* REGISTER */}
+        <TabsContent value="register">
+          <Card>
+            <CardHeader>
+              <CardTitle>Criar conta</CardTitle>
+              <CardDescription>Crie uma conta para continuar.</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nome</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Seu nome" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="seu@email.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Senha</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit" className="w-full">
+                    Criar conta
+                  </Button>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
