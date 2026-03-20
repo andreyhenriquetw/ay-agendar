4ebda141ad0b4314d1341ffe64d3f4a0792a4276
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-03-19 21:50:07 -0300

    feat: adicionar botão de sair e melhorar a página do dashboard

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,10 +1,18 @@
-"use client";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignUpForm from "./components/sign-up-form";
 import LoginForm from "./components/login-form";
+import { auth } from "@/lib/auth";
 
-const AuthenticationPage = () => {
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex min-h-screen w-full items-center justify-center px-4 py-10">
       <Tabs defaultValue="login" className="w-full max-w-md">
